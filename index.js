c44a95514b33f338c34c955367875abc410ab1c7
HrithikMittal <adhikanshmittal@gmail.com>
2019-08-11 08:44:56 +0530

    add path to the id

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var mongoose = require("mongoose");
 var express = require("express");
 var bodyparser = require("body-parser");
+var cors = require("cors");
 var app = express();
 
 var User = require("./routes/User");
 var Question = require("./routes/Question");
 var Test = require("./routes/test");
+
+const port = 3010;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyparser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyparser.json());
 
 mongoose
   .connect("mongodb://localhost:27017/quiz")
@@ -16,14 +25,15 @@ mongoose
     console.log("Error is ", err.message);
   });
 
-app.use(bodyparser.urlencoded({ extended: false }));
-app.use(bodyparser.json());
+app.use(cors());
 
-const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Hi I am Muskaan Chutiya");
+});
 
 app.use("/question", Question);
 app.use("/user", User);
-app.use("/test",Test);
+app.use("/test", Test);
 
 app.listen(port, () => {
   console.log(`Servering is listening on PORT ${port}`);
